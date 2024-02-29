e23ef0162f7de99fcb39ab7dacbcfb1e9d488b12
dev_mikeyy <161330407+devmikeyy@users.noreply.github.com>
2024-02-29 22:29:48 +0530

    helloworld.js
    Basic .js code (Node.js)

---
+++ helloworld.js
@@ -0,0 +1,23 @@
+function nodeJSConsole() {
+    console.log("Hello, World from Node.js!");
+}
+
+
+function browserConsole() {
+    console.log("Hello, World from Browser Console!");
+}
+
+
+function main() {
+   
+    if (typeof module !== 'undefined' && module.exports) {
+        // Running in Node.js
+        nodeJSConsole();
+    } else {
+        
+        browserConsole();
+    }
+}
+
+
+main();
